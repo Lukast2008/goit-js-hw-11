026e23f07e1617c17ce0d69dcc607050f8f0a3a6
Lukast2008 <lukast2008@gmail.com>
2022-11-11 11:59:53 +0200

    add style

--- src/markup/markup.js
+++ src/markup/markup.js
@@ -10,25 +10,28 @@ export function fillListImage(data) {
         comments,
         downloads,
       }) => {
-        return `<div class="photo-card">
-                  <a class="gallery" href="${largeImageURL}">
-                    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                  </a>
-                  <div class="info">
-                    <p class="info-item">
-                      <b>Likes</b>${likes}
-                    </p>
-                    <p class="info-item">
-                      <b>Views</b>${views}
-                    </p>
-                    <p class="info-item">
-                      <b>Comments</b>${comments}
-                    </p>
-                    <p class="info-item">
-                      <b>Downloads</b>${downloads}
-                    </p>
+        return `<a class="gallery-item" href="${largeImageURL}">
+                  <div class="photo-card">
+                  
+                    <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+                 
+                    <div class="info">
+                      <p class="info-item">
+                        <b>Likes</b>${likes}
+                      </p>
+                      <p class="info-item">
+                        <b>Views</b>${views}
+                      </p>
+                      <p class="info-item">
+                        <b>Comments</b>${comments}
+                      </p>
+                      <p class="info-item">
+                        <b>Downloads</b>${downloads}
+                      </p>
+                    </div>
+                 
                   </div>
-                </div>`;
+                </a>`;
       }
     )
     .join('');
